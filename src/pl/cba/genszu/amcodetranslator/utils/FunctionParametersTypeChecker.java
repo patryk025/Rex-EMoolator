6d9bab60381412990aac0fd68738b94ed623dbbd
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2023-08-13 18:56:24 +0200

    Added new Exception for invalid param type, started creating parameters type checker

---
+++ src/pl/cba/genszu/amcodetranslator/utils/FunctionParametersTypeChecker.java
@@ -0,0 +1,28 @@
+package pl.cba.genszu.amcodetranslator.utils;
+
+import pl.cba.genszu.amcodetranslator.interpreter.Variable;
+import pl.cba.genszu.amcodetranslator.interpreter.exceptions.InterpreterException;
+import pl.cba.genszu.amcodetranslator.interpreter.exceptions.InvalidMethodParamTypeException;
+
+public class FunctionParametersTypeChecker {
+    public static void checkTypes(Variable[] params, String[] types) {
+        int requiredParams = 0;
+        for(String type : types) {
+            if(!type.startsWith("[") && !type.endsWith("]"))
+                requiredParams++;
+        }
+        if(params.length < requiredParams)
+            throw new InterpreterException(String.format("Oczekiwano %d wymaganych argumentów, a otrzymano %d.", requiredParams, params.length));
+        //TODO: obsługa opcjonalnych parametrów w środku listy
+        for (int i = 0; i < params.length; i++) {
+            boolean required = !types[i].startsWith("[") && !types[i].endsWith("]");
+            if (!required) {
+                types[i] = types[i].substring(1, types[i].length()-1);
+            }
+            String paramType = params[i].getType();
+            if (!paramType.equals(types[i])) {
+                throw new InvalidMethodParamTypeException(types[i], paramType);
+            }
+        }
+    }
+}
