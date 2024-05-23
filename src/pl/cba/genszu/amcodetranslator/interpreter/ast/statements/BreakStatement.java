04014773a4b899e97a035dac986dc5fc524744e9
patryk025 <43010113+patryk025@users.noreply.github.com>
2024-05-23 08:55:53 +0200

    Added break statements

---
+++ src/pl/cba/genszu/amcodetranslator/interpreter/ast/statements/BreakStatement.java
@@ -0,0 +1,14 @@
+package pl.cba.genszu.amcodetranslator.interpreter.ast.statements;
+import pl.cba.genszu.amcodetranslator.interpreter.ast.*;
+import pl.cba.genszu.amcodetranslator.interpreter.*;
+
+public class BreakStatement extends Statement
+{
+	
+	@Override
+	public void execute(Context context)
+	{
+		// That class represents @BREAK, so technically does nothing
+	}
+	
+}
---
+++ src/pl/cba/genszu/amcodetranslator/interpreter/ast/statements/OneBreakStatement.java
@@ -0,0 +1,15 @@
+package pl.cba.genszu.amcodetranslator.interpreter.ast.statements;
+
+import pl.cba.genszu.amcodetranslator.interpreter.*;
+import pl.cba.genszu.amcodetranslator.interpreter.ast.*;
+
+public class OneBreakStatement extends Statement
+{
+
+	@Override
+	public void execute(Context context)
+	{
+		// That class represents @ONEBREAK, so technically does nothing
+	}
+
+}
