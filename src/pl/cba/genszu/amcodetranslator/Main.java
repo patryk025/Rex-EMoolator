fe45ca862fe0027129c4ca4ed8138465c9ead260
patryk025 <43010113+patryk025@users.noreply.github.com>
2024-07-01 19:55:50 +0200

    Whatever

--- src/pl/cba/genszu/amcodetranslator/Main.java
+++ src/pl/cba/genszu/amcodetranslator/Main.java
@@ -27,11 +27,22 @@ public class Main
 		//String testExpression = "{@INT(TEST,1);@DOUBLE(TEST2,2.1);@STRING(TEST3,\"TEST\");@BOOL(TEST4,TRUE);@IF(\"TEST4'TRUE\", {[TEST+TEST2+\"2\"+TEST3]}, {});}";
 		//String testExpression = "{@CONV(ITERATOR, \"BOOL\");}";
 		//String testExpression = "{ITERATOR^SET(5);@STRING(STRING_TEST,[TEST+ITERATOR]);@RETURN(STRING_TEST);}";
-		String testExpression = "{TMPHOLDER^SET([TEST_DODAWANIA+\"|\"+BOOL_Z_BOOLEM__+[TEST2+TEST2_COMPARE]+\"|\"+BOOL_ZE_STRINGIEM__+[TEST2+TEST_COMPARE]+\"|\"+BOOL_Z_INTEGEREM__+[TEST2+TEST4_COMPARE]+\"|\"+BOOL_Z_DOUBLEM__+[TEST2+TEST3_COMPARE]+\"|\"+STRING_ZE_STRINGIEM__+[TEST+TEST_COMPARE]+\"|\"+STRING_Z_INTEGEREM__+[TEST+TEST4_COMPARE]+\"|\"+STRING_Z_DOUBLEM__+[TEST+TEST3_COMPARE]+\"|\"+STRING_Z_BOOLEM__+[TEST+TEST2_COMPARE]+\"|\"+INTEGER_ZE_STRINGIEM__+[TEST4+TEST_COMPARE]+\"|\"+INTEGER_Z_INTEGEREM__+[TEST4+TEST4_COMPARE]+\"|\"+INTEGER_Z_DOUBLEM__+[TEST4+TEST3_COMPARE]+\"|\"+INTEGER_Z_BOOLEM__+[TEST4+TEST2_COMPARE]+\"|\"+DOUBLE_ZE_STRINGIEM__+[TEST3+TEST_COMPARE]+\"|\"+DOUBLE_Z_INTEGEREM__+[TEST3+TEST4_COMPARE]+\"|\"+DOUBLE_Z_DOUBLEM__+[TEST3+TEST3_COMPARE]+\"|\"+DOUBLE_Z_BOOLEM__+[TEST3+TEST2_COMPARE]]);}";
-
+		//String testExpression = "{TMPHOLDER^SET([TEST_DODAWANIA+\"|\"+BOOL_Z_BOOLEM__+[TEST2+TEST2_COMPARE]+\"|\"+BOOL_ZE_STRINGIEM__+[TEST2+TEST_COMPARE]+\"|\"+BOOL_Z_INTEGEREM__+[TEST2+TEST4_COMPARE]+\"|\"+BOOL_Z_DOUBLEM__+[TEST2+TEST3_COMPARE]+\"|\"+STRING_ZE_STRINGIEM__+[TEST+TEST_COMPARE]+\"|\"+STRING_Z_INTEGEREM__+[TEST+TEST4_COMPARE]+\"|\"+STRING_Z_DOUBLEM__+[TEST+TEST3_COMPARE]+\"|\"+STRING_Z_BOOLEM__+[TEST+TEST2_COMPARE]+\"|\"+INTEGER_ZE_STRINGIEM__+[TEST4+TEST_COMPARE]+\"|\"+INTEGER_Z_INTEGEREM__+[TEST4+TEST4_COMPARE]+\"|\"+INTEGER_Z_DOUBLEM__+[TEST4+TEST3_COMPARE]+\"|\"+INTEGER_Z_BOOLEM__+[TEST4+TEST2_COMPARE]+\"|\"+DOUBLE_ZE_STRINGIEM__+[TEST3+TEST_COMPARE]+\"|\"+DOUBLE_Z_INTEGEREM__+[TEST3+TEST4_COMPARE]+\"|\"+DOUBLE_Z_DOUBLEM__+[TEST3+TEST3_COMPARE]+\"|\"+DOUBLE_Z_BOOLEM__+[TEST3+TEST2_COMPARE]]);}";
+		String testExpression = "{@RETURN([TEST^ADD(TEST_COMPARE)]);}";
+		
 		Context context = new Context();
 		context.setVariable("TEST", new StringVariable("TEST", "ANIMO_", context));
 		context.setVariable("ITERATOR", new IntegerVariable("ITERATOR", 3, context));
+		
+		context.setVariable("TEST", new StringVariable("TEST", "Test", context));
+		context.setVariable("TEST2", new BoolVariable("TEST2", true, context));
+		context.setVariable("TEST3", new DoubleVariable("TEST3", 1.5, context));
+		context.setVariable("TEST4", new IntegerVariable("TEST4", 1, context));
+		
+		context.setVariable("TEST_COMPARE", new StringVariable("TEST_COMPARE", "3", context));
+		context.setVariable("TEST2_COMPARE", new IntegerVariable("TEST2_COMPARE", 2, context));
+		context.setVariable("TEST3_COMPARE", new DoubleVariable("TEST3_COMPARE", -2.5, context));
+		context.setVariable("TEST4_COMPARE", new IntegerVariable("TEST4_COMPARE", -3, context));
 		
 		context.setVariable("TMPHOLDER", new StringVariable("TMPHOLDER", "", context));
 
@@ -45,7 +56,7 @@ public class Main
 
 		Interpreter interpreter2 = new Interpreter(astRoot, context);
 		interpreter2.interpret();
-		//System.out.println("Returned value: " + interpreter2.getReturnValue());
-		System.out.println(((String) context.getVariable("TMPHOLDER").getValue()).replace("|", "\n"));
+		System.out.println("Returned value: " + interpreter2.getReturnValue());
+		//System.out.println(((String) context.getVariable("TMPHOLDER").getValue()).replace("|", "\n"));
 	}
 }
