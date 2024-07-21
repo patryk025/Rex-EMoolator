f20810de69369a3a340f8f83eb997af83ad05395
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2024-07-21 19:52:37 +0200

    Modified context for variable lookup in parents

--- core/src/pl/genschu/bloomooemulator/interpreter/Context.java
+++ core/src/pl/genschu/bloomooemulator/interpreter/Context.java
@@ -7,11 +7,24 @@ import java.util.Map;
 
 public class Context {
     private Map<String, Variable> variables = new HashMap<>();
-	private String sceneName = "BRAKSCENY";
+    private Context parentContext;
+    private String sceneName = "BRAKSCENY";
     private Object returnValue;
 
+    public Context() {
+        this.parentContext = null;
+    }
+
+    public Context(Context parentContext) {
+        this.parentContext = parentContext;
+    }
+
     public Variable getVariable(String name) {
-        return variables.get(name);
+        Variable variable = variables.get(name);
+        if (variable == null && parentContext != null) {
+            return parentContext.getVariable(name);
+        }
+        return variable;
     }
 
     public void setVariable(String name, Variable variable) {
@@ -19,28 +32,37 @@ public class Context {
     }
 
     public boolean hasVariable(String name) {
-        return variables.containsKey(name);
+        if (variables.containsKey(name)) {
+            return true;
+        }
+        return parentContext != null && parentContext.hasVariable(name);
     }
 
     public void removeVariable(String name) {
         variables.remove(name);
     }
-	
-	public void setSceneName(String sceneName)
-	{
-		this.sceneName = sceneName;
-	}
 
-	public String getSceneName()
-	{
-		return sceneName;
-	}
+    public void setSceneName(String sceneName) {
+        this.sceneName = sceneName;
+    }
+
+    public String getSceneName() {
+        return sceneName;
+    }
 
     public void setReturnValue(Object returnValue) {
         this.returnValue = returnValue;
     }
+
     public Object getReturnValue() {
         return returnValue;
     }
-}
 
+    public Context getParentContext() {
+        return parentContext;
+    }
+
+    public void setParentContext(Context parentContext) {
+        this.parentContext = parentContext;
+    }
+}
