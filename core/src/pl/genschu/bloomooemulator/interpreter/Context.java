patryk025/Rex-EMoolator

commit 1cd1f23dfa2c3db197e270ff88d31bbb5f188c02

Working mouse event bubbling
Currently, there are problems with flickering buttons

core/src/pl/genschu/bloomooemulator/interpreter/Context.java
@@ -22,6 +22,7 @@ public class Context {
     private final Map<String, Variable> soundVariables;
     private final Map<String, Variable> classInstances;
     private MouseVariable mouseVariable;
+    private List<MouseVariable> mouseVariables;
     private KeyboardVariable keyboardVariable;
 
     Variable thisVariable = null;
@@ -35,6 +36,7 @@ public class Context {
         this.soundVariables = new LinkedHashMap<>();
         this.classInstances = new LinkedHashMap<>();
         this.mouseVariable = null;
+        this.mouseVariables = new ArrayList<>();
         this.keyboardVariable = null;
     }
 
@@ -47,6 +49,7 @@ public class Context {
         this.soundVariables = new LinkedHashMap<>();
         this.classInstances = new LinkedHashMap<>();
         this.mouseVariable = null;
+        this.mouseVariables = new ArrayList<>();
         this.keyboardVariable = null;
     }
 
@@ -242,6 +245,22 @@ public class Context {
         return variables;
     }
 
+    public void refreshDevicesVariablesList() {
+        // Create mouse and keyboard variables hierarchy
+        mouseVariables.clear();
+        if(mouseVariable != null) {
+            mouseVariables.add(mouseVariable);
+        }
+        Context tmpContext = parentContext;
+        while(tmpContext != null) {
+            if(tmpContext.mouseVariable != null) {
+                mouseVariables.add(tmpContext.mouseVariable);
+            }
+            tmpContext = tmpContext.parentContext;
+        }
+        mouseVariables = mouseVariables.reversed();
+    }
+
     public Variable getThisVariable() {
         if(parentContext != null)
             return parentContext.getThisVariable();
@@ -266,6 +285,10 @@ public class Context {
 
     public void setMouseVariable(MouseVariable mouseVariable) {
         this.mouseVariable = mouseVariable;
+    }
+
+    public List<MouseVariable> getMouseVariables() {
+        return mouseVariables;
     }
 
     public KeyboardVariable getKeyboardVariable() {

core/src/pl/genschu/bloomooemulator/interpreter/variable/types/ButtonVariable.java
@@ -276,6 +276,10 @@ public class ButtonVariable extends Variable {
 		return isVisible;
 	}
 
+	public void setVisible(boolean visible) {
+		isVisible = visible;
+	}
+
 	public void setEnabled(boolean enabled) {
 		isEnabled = enabled;
 	}
@@ -394,7 +398,8 @@ public class ButtonVariable extends Variable {
                     isEnabled = attribute.getValue().toString().equals("TRUE");
                     if (!isEnabled) {
                         Gdx.app.debug("ButtonVariable", "Disabling button and hiding images: " + getName());
-                        hideImages();
+						isVisible = false; // z testów wynika, że ENABLE = FALSE powoduje nadpisanie VISIBLE = TRUE
+						hideImages();
                     }
                     break;
                 case "VISIBLE":

core/src/pl/genschu/bloomooemulator/loader/CNVParser.java
@@ -16,10 +16,7 @@ import pl.genschu.bloomooemulator.interpreter.variable.Variable;
 import pl.genschu.bloomooemulator.interpreter.factories.VariableFactory;
 import pl.genschu.bloomooemulator.encoding.ScriptDecypher;
 import com.badlogic.gdx.Gdx;
-import pl.genschu.bloomooemulator.interpreter.variable.types.BehaviourVariable;
-import pl.genschu.bloomooemulator.interpreter.variable.types.ConditionVariable;
-import pl.genschu.bloomooemulator.interpreter.variable.types.SequenceVariable;
-import pl.genschu.bloomooemulator.interpreter.variable.types.SoundVariable;
+import pl.genschu.bloomooemulator.interpreter.variable.types.*;
 import pl.genschu.bloomooemulator.utils.SignalAndParams;
 
 import java.io.*;
@@ -82,7 +79,7 @@ public class CNVParser {
                     objects.put(currentObjectName, currentObject);
                 }
                 currentObjectName = line.split(separator)[1];
-                currentObject = new HashMap<>();
+                currentObject = new LinkedHashMap<>();
             } else if (currentObjectName != null) {
                 String[] parts = line.split(separator, 2);
                 if (parts.length == 2) {
@@ -143,6 +140,15 @@ public class CNVParser {
                         String signalCode = property.getValue();
                         variable.addPendingSignal(signalName, signalCode);
                     }
+                }
+            }
+
+            if(type.equals("BUTTON")) {
+                ButtonVariable buttonVariable = (ButtonVariable) variable;
+                if(!buttonVariable.isEnabled()) {
+                    Gdx.app.debug("ButtonVariable", "Disabling button and hiding images: " + buttonVariable.getName());
+                    buttonVariable.setVisible(false); // z testów wynika, że ENABLE = FALSE powoduje nadpisanie VISIBLE = TRUE
+                    buttonVariable.hideImages();
                 }
             }
 

core/src/pl/genschu/bloomooemulator/BlooMooEmulator.java
@@ -97,6 +97,7 @@ public class BlooMooEmulator extends ApplicationAdapter {
         List<Variable> drawList = getGraphicsVariables();
 
         MouseVariable mouseVariable = context.getMouseVariable();
+        List<MouseVariable> mouseVariables = context.getMouseVariables();
         /*if(mouseVariable != null) { // to w przyszłości
             drawList.add(mouseVariable);
         }*/
@@ -118,8 +119,8 @@ public class BlooMooEmulator extends ApplicationAdapter {
             x = (int) correctedVector.x;
             y = (int) correctedVector.y;
 
-            if (mouseVariable != null)
-                mouseVariable.update(x, y);
+            for(MouseVariable mouse : mouseVariables)
+                mouse.update(x, y);
 
             handleMouseInput(x, y, isPressed, justPressed, justReleased, mouseVariable, new ArrayList<>(drawList));
 
@@ -525,12 +526,13 @@ public class BlooMooEmulator extends ApplicationAdapter {
         Variable graphics = getGraphicsAt(x, y, drawList, false);
 
         //Gdx.app.log("Mouse", "mouseVariable != null: " + (mouseVariable != null) + " isPressed: " + isPressed + " mouseVariable.isEnabled(): " + (mouseVariable != null && mouseVariable.isEnabled()));
-        if(mouseVariable != null && mouseVariable.isEnabled()) {
-            if(justPressed) {
-                mouseVariable.emitSignal("ONCLICK", "LEFT"); // right is not used at all
-            }
-            else if(justReleased) {
-                mouseVariable.emitSignal("ONRELEASE", "LEFT"); // right is not used at all
+        if(!context.getMouseVariables().isEmpty()) {
+            for(MouseVariable mouse : context.getMouseVariables()) {
+                if (justPressed) {
+                    mouse.emitSignal("ONCLICK", "LEFT"); // right is not used at all
+                } else if (justReleased) {
+                    mouse.emitSignal("ONRELEASE", "LEFT"); // right is not used at all
+                }
             }
         }
 
@@ -564,6 +566,12 @@ public class BlooMooEmulator extends ApplicationAdapter {
             } else {
                 showTooltip = false;
             }
+        }
+    }
+
+    private void triggerMouseEvent(String event, String argument) {
+        for(MouseVariable mouse : context.getMouseVariables()) {
+
         }
     }
 
@@ -706,49 +714,48 @@ public class BlooMooEmulator extends ApplicationAdapter {
             return Integer.compare(priority1, priority2);
         };
         Collections.sort(drawList, comparator);
-        Gdx.app.log("Draw list after sort", getDrawListAsString(drawList));
+        //Gdx.app.log("Draw list after sort", getDrawListAsString(drawList));
         return drawList;
     }
 
-private String getDrawListAsString(List<Variable> drawList) {
-    StringBuilder sb = new StringBuilder();
-    for (Variable variable : drawList) {
-        boolean visible = false;
-        if (variable instanceof ImageVariable) {
-            visible = ((ImageVariable) variable).isVisible();
-        }
-        if (variable instanceof AnimoVariable) {
-            visible = ((AnimoVariable) variable).isVisible();
-        }
-        if (variable instanceof SequenceVariable) {
-            visible = ((SequenceVariable) variable).isVisible();
-        }
-        if (!visible) {
-            continue;
-        }
-
-        if (variable instanceof SequenceVariable) {
-            /*SequenceVariable seqVar = (SequenceVariable) variable;
-            if(seqVar.getCurrentEventName() != null) {
-                if (seqVar.getCurrentEventName().isEmpty()) {
-                    sb.append("__").append(seqVar.getName()).append("_ANIMO_");
-                }
-            }*/
-        } else {
-            sb.append(variable.getName());
-            Attribute priority = variable.getAttribute("PRIORITY");
-            if (priority != null) {
-                sb.append(" (").append(priority.getValue()).append(")");
+    private String getDrawListAsString(List<Variable> drawList) {
+        StringBuilder sb = new StringBuilder();
+        for (Variable variable : drawList) {
+            boolean visible = false;
+            if (variable instanceof ImageVariable) {
+                visible = ((ImageVariable) variable).isVisible();
             }
-            sb.append(", ");
-        }
-    }
-    if (sb.length() > 2) {
-        sb.setLength(sb.length() - 2);
-    }
-    return sb.toString();
-}
+            if (variable instanceof AnimoVariable) {
+                visible = ((AnimoVariable) variable).isVisible();
+            }
+            if (variable instanceof SequenceVariable) {
+                visible = ((SequenceVariable) variable).isVisible();
+            }
+            if (!visible) {
+                continue;
+            }
 
+            if (variable instanceof SequenceVariable) {
+                /*SequenceVariable seqVar = (SequenceVariable) variable;
+                if(seqVar.getCurrentEventName() != null) {
+                    if (seqVar.getCurrentEventName().isEmpty()) {
+                        sb.append("__").append(seqVar.getName()).append("_ANIMO_");
+                    }
+                }*/
+            } else {
+                sb.append(variable.getName());
+                Attribute priority = variable.getAttribute("PRIORITY");
+                if (priority != null) {
+                    sb.append(" (").append(priority.getValue()).append(")");
+                }
+                sb.append(", ");
+            }
+        }
+        if (sb.length() > 2) {
+            sb.setLength(sb.length() - 2);
+        }
+        return sb.toString();
+    }
 
     @Override
     public void resize(int width, int height) {

core/src/pl/genschu/bloomooemulator/objects/Game.java
@@ -391,6 +391,8 @@ public class Game {
                 scene.getMusic().play();
             }
 
+            currentSceneContext.refreshDevicesVariablesList();
+
             runInit(currentSceneContext);
         } catch (IOException e) {
             throw new RuntimeException(e);

commit f7dbaaf7b0ea7d8ddacbd0fd55df5525e98b81f0

Partially fixed regression caused by 1cd1f23dfa2c3db197e270ff88d31bbb5f188c02

core/src/pl/genschu/bloomooemulator/interpreter/Context.java
@@ -300,7 +300,11 @@ public class Context {
     }
 
     public MouseVariable getMouseVariable() {
-        return mouseVariable;
+        if(mouseVariable != null)
+            return mouseVariable;
+        else {
+            return parentContext != null ? parentContext.getMouseVariable() : (MouseVariable) GlobalVariables.getVariable("MOUSE", this);
+        }
     }
 
     public void setMouseVariable(MouseVariable mouseVariable) {
@@ -308,7 +312,11 @@ public class Context {
     }
 
     public KeyboardVariable getKeyboardVariable() {
-        return keyboardVariable;
+        if(keyboardVariable != null)
+            return keyboardVariable;
+        else {
+            return parentContext != null ? parentContext.getKeyboardVariable() : (KeyboardVariable) GlobalVariables.getVariable("KEYBOARD", this);
+        }
     }
 
     public void setKeyboardVariable(KeyboardVariable keyboardVariable) {

core/src/pl/genschu/bloomooemulator/interpreter/variable/types/ButtonVariable.java
@@ -309,9 +309,6 @@ public class ButtonVariable extends Variable {
 		if(gfxOnClick != null) {
 			gfxOnClick.setAttribute("VISIBLE", new Attribute("BOOL", "FALSE"));
 		}
-		if(rectVariable != null) {
-			rectVariable.setAttribute("VISIBLE", new Attribute("BOOL", "FALSE"));
-		}
 		currentGfx = gfxVariable;
 	}
 
@@ -351,10 +348,6 @@ public class ButtonVariable extends Variable {
 	}
 
 	public void changeGraphicsVisibility(boolean visible) {
-		if(rectVariable != null) {
-			showImage(rectVariable, visible);
-		}
-
 		if(gfxVariable != null) {
 			showImage(gfxVariable, visible);
 		}
@@ -396,11 +389,6 @@ public class ButtonVariable extends Variable {
             switch (name) {
                 case "ENABLE":
                     isEnabled = attribute.getValue().toString().equals("TRUE");
-                    if (!isEnabled) {
-                        Gdx.app.debug("ButtonVariable", "Disabling button and hiding images: " + getName());
-						isVisible = false; // z testów wynika, że ENABLE = FALSE powoduje nadpisanie VISIBLE = TRUE
-						hideImages();
-                    }
                     break;
                 case "VISIBLE":
                     isVisible = attribute.getValue().toString().equals("TRUE");

core/src/pl/genschu/bloomooemulator/loader/CNVParser.java
@@ -164,15 +164,6 @@ public class CNVParser {
 
             variable.setIniSection(iniSection);
 
-            if(type.equals("BUTTON")) {
-                ButtonVariable buttonVariable = (ButtonVariable) variable;
-                if(!buttonVariable.isEnabled()) {
-                    Gdx.app.debug("ButtonVariable", "Disabling button and hiding images: " + buttonVariable.getName());
-                    buttonVariable.setVisible(false); // z testów wynika, że ENABLE = FALSE powoduje nadpisanie VISIBLE = TRUE
-                    buttonVariable.hideImages();
-                }
-            }
-
             context.setVariable(objectName, variable);
         } catch (IllegalArgumentException e) {
             Gdx.app.error("CNVParser", "Failed to create variable " + objectName + ": " + e.getMessage());