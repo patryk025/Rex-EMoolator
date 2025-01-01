2d0dc171cbf98ed78b6926b4eb2a969f8fa075de
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2025-01-01 22:03:28 +0100

    Added catch NPE in BlockExpression

--- core/src/pl/genschu/bloomooemulator/interpreter/ast/expressions/BlockExpression.java
+++ core/src/pl/genschu/bloomooemulator/interpreter/ast/expressions/BlockExpression.java
@@ -35,7 +35,7 @@ public class BlockExpression extends Expression {
                         }
                     }
                 }
-                catch(ClassMethodNotFoundException e) {
+                catch(ClassMethodNotFoundException | NullPointerException e) {
                     Gdx.app.error("BlockExpression", "Method call error: " + e.getMessage(), e);
                 }
             }
