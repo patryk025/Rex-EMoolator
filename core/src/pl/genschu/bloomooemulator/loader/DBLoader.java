bbe426f8c4e12c62e58ea0ba2bb7142e1db06c90
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2025-03-17 22:46:25 +0100

    Removed checks when line is empty and if there is no values in DBLoader
    BlooMoo loads data as it is, it doesn't care about if line is empty or columns doesn't match. Load, don't check, script do the rest.

--- core/src/pl/genschu/bloomooemulator/loader/DBLoader.java
+++ core/src/pl/genschu/bloomooemulator/loader/DBLoader.java
@@ -20,22 +20,12 @@ public class DBLoader {
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line;
             while ((line = reader.readLine()) != null) {
-                if(line.startsWith("#")) continue;
-                if(line.isEmpty()) continue;
-
                 String[] values = line.split("\\|");
 
-                // make sure that values size matches column size
-                if(values.length < 1) {
-                    Gdx.app.log("DBLoader", "Skipping line: " + line + " (missing values)");
-                }
                 if(variable.getColumns() == null) {
                     Gdx.app.error("DBLoader", "Missing model in database. Aborting...");
                     return;
                 }
-                /*if(variable.getColumns().getFields().size() != values.length) {
-                    Gdx.app.log("DBLoader", "Skipping line: " + line + " (wrong number of columns)");
-                }*/
 
                 List<String> row = new ArrayList<>(Arrays.asList(values));
                 data.add(row);
