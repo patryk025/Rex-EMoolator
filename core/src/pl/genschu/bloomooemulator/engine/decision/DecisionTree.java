99f7e925b98df6d8d488366a9cb95fdff85b3abf
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2025-04-24 15:19:05 +0200

    Added class for decision tree

---
+++ core/src/pl/genschu/bloomooemulator/engine/decision/DecisionTree.java
@@ -0,0 +1,54 @@
+package pl.genschu.bloomooemulator.engine.decision;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class DecisionTree<T> {
+    private final DecisionNode<T> root;
+
+    public DecisionTree(DecisionNode<T> root) {
+        this.root = root;
+    }
+
+    public T evaluate(Object... context) {
+        return root.evaluate(context);
+    }
+
+    public interface DecisionNode<T> {
+        T evaluate(Object... context);
+    }
+
+    public static class ConditionNode<T> implements DecisionNode<T> {
+        private final Predicate<Object[]> condition;
+        private final DecisionNode<T> trueNode;
+        private final DecisionNode<T> falseNode;
+
+        public ConditionNode(Predicate<Object[]> condition, DecisionNode<T> trueNode, DecisionNode<T> falseNode) {
+            this.condition = condition;
+            this.trueNode = trueNode;
+            this.falseNode = falseNode;
+        }
+
+        @Override
+        public T evaluate(Object... context) {
+            if (condition.test(context)) {
+                return trueNode.evaluate(context);
+            } else {
+                return falseNode.evaluate(context);
+            }
+        }
+    }
+
+    public static class ActionNode<T> implements DecisionNode<T> {
+        private final Function<Object[], T> action;
+
+        public ActionNode(Function<Object[], T> action) {
+            this.action = action;
+        }
+
+        @Override
+        public T evaluate(Object... context) {
+            return action.apply(context);
+        }
+    }
+}
