patryk025/Rex-EMoolator

commit e89a568551b963fe0d436bfc7b1c733462ee1ff1

Working on events in sequence
I'm still working on it, how events are fired

core/src/pl/genschu/bloomooemulator/interpreter/variable/types/SequenceVariable.java
@@ -349,6 +349,7 @@ public class SequenceVariable extends Variable {
 			stopSequence(false); // Stop current sequence
 			currentEvent = event;
 			isPlaying = true;
+			emitSignal("ONSTARTED", event.name);
 			startEvent(event);
 		}
 	}
@@ -561,7 +562,6 @@ public class SequenceVariable extends Variable {
 	private void startSequentialEvent(SequenceEvent event) {
 		Gdx.app.log("SequenceVariable", "startSequentialEvent: " + event.name);
 		if (!event.subEvents.isEmpty()) {
-			emitSignal("ONSTARTED", event.name);
 			startEvent(event.subEvents.get(0));
 		}
 	}
@@ -588,28 +588,20 @@ public class SequenceVariable extends Variable {
 			int currentIndex = parentEvent.subEvents.indexOf(event);
 			if (parentEvent.mode == SequenceMode.SEQUENCE &&
 					currentIndex < parentEvent.subEvents.size() - 1) {
-				emitSignal("ONFINISHED", event.name);
 				// Next event, play it
 				startEvent(parentEvent.subEvents.get(currentIndex + 1));
 			} else {
 				// End of event
 				stopEvent(parentEvent);
-				if (parentEvent.mode == SequenceMode.SEQUENCE &&
-						currentIndex == parentEvent.subEvents.size() - 1) {
-					emitSignal("ONFINISHED", event.name);
-					emitSignal("ONFINISHED", parentEvent.name);
-				}
-				else {
-					emitSignal("ONFINISHED", event.name);
-				}
-				if (parentEvent.parent == null) {
+                emitSignal("ONFINISHED", currentEvent.name);
+                if (parentEvent.parent == null) {
 					isPlaying = false;
 				}
 			}
 		} else {
 			// End of sequence
 			stopEvent(event);
-			emitSignal("ONFINISHED", event.name);
+			emitSignal("ONFINISHED", currentEvent.name);
 			isPlaying = false;
 		}
 	}

commit 8ac0853ebdc01e7fa3c20f32172c2c951f949b80

Returned to SequenceVariable, fixing regression caused by e89a568 (at least in Reksio i Wehikuł Czasu)

core/src/pl/genschu/bloomooemulator/interpreter/variable/types/SequenceVariable.java
@@ -582,12 +582,20 @@ public class SequenceVariable extends Variable {
 			int currentIndex = parentEvent.subEvents.indexOf(event);
 			if (parentEvent.mode == SequenceMode.SEQUENCE &&
 					currentIndex < parentEvent.subEvents.size() - 1) {
+				emitSignal("ONFINISHED", event.name);
 				// Next event, play it
 				startEvent(parentEvent.subEvents.get(currentIndex + 1));
 			} else {
 				// End of event
 				stopEvent(parentEvent);
-                emitSignal("ONFINISHED", currentEvent.name);
+				if (parentEvent.mode == SequenceMode.SEQUENCE &&
+						currentIndex == parentEvent.subEvents.size() - 1) {
+					emitSignal("ONFINISHED", event.name);
+					emitSignal("ONFINISHED", parentEvent.name);
+				}
+				else {
+					emitSignal("ONFINISHED", event.name);
+				}
                 if (parentEvent.parent == null) {
 					isPlaying = false;
 				}