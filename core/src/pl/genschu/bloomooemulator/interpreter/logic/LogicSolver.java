18356b04cddb252893083d8969f9b8e23e5f6072
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2025-05-07 13:44:48 +0200

    Small tweak in LogicSolver
    It ensures that comparison occurs only between primitive types (Piklib seems like it's casting incompatible variables to string, like undefined variable).

--- core/src/pl/genschu/bloomooemulator/interpreter/logic/LogicSolver.java
+++ core/src/pl/genschu/bloomooemulator/interpreter/logic/LogicSolver.java
@@ -2,6 +2,7 @@ package pl.genschu.bloomooemulator.interpreter.logic;
 
 import pl.genschu.bloomooemulator.interpreter.variable.Variable;
 import pl.genschu.bloomooemulator.interpreter.logic.operations.*;
+import pl.genschu.bloomooemulator.interpreter.variable.types.StringVariable;
 
 public class LogicSolver {
     static AndOperation andOperation = new AndOperation();
@@ -14,34 +15,41 @@ public class LogicSolver {
     static LessOrEqualsOperation lessOrEqualsOperation = new LessOrEqualsOperation();
 
     public static Variable and(Variable var1, Variable var2) {
-        return andOperation.performOperation(var1, var2);
+        return andOperation.performOperation(ensurePrimitive(var1), ensurePrimitive(var2));
     }
 
     public static Variable or(Variable var1, Variable var2) {
-        return orOperation.performOperation(var1, var2);
+        return orOperation.performOperation(ensurePrimitive(var1), ensurePrimitive(var2));
     }
 
     public static Variable equals(Variable var1, Variable var2) {
-        return equalsOperation.performOperation(var1, var2);
+        return equalsOperation.performOperation(ensurePrimitive(var1), ensurePrimitive(var2));
     }
 
     public static Variable notEquals(Variable var1, Variable var2) {
-        return notEqualsOperation.performOperation(var1, var2);
+        return notEqualsOperation.performOperation(ensurePrimitive(var1), ensurePrimitive(var2));
     }
 
     public static Variable greater(Variable var1, Variable var2) {
-        return greaterOperation.performOperation(var1, var2);
+        return greaterOperation.performOperation(ensurePrimitive(var1), ensurePrimitive(var2));
     }
 
     public static Variable greaterOrEquals(Variable var1, Variable var2) {
-        return greaterOrEqualsOperation.performOperation(var1, var2);
+        return greaterOrEqualsOperation.performOperation(ensurePrimitive(var1), ensurePrimitive(var2));
     }
 
     public static Variable less(Variable var1, Variable var2) {
-        return lessOperation.performOperation(var1, var2);
+        return lessOperation.performOperation(ensurePrimitive(var1), ensurePrimitive(var2));
     }
 
     public static Variable lessOrEquals(Variable var1, Variable var2) {
-        return lessOrEqualsOperation.performOperation(var1, var2);
+        return lessOrEqualsOperation.performOperation(ensurePrimitive(var1), ensurePrimitive(var2));
+    }
+
+    private static Variable ensurePrimitive(Variable var) {
+        if(var.getType().matches("STRING|INTEGER|DOUBLE|BOOL"))
+            return var;
+        else
+            return new StringVariable("", var.getName(), var.getContext());
     }
 }
