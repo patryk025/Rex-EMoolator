f53c97d523e847a871504351bba965b33660cb5b
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2025-06-20 13:18:13 +0200

    test: Added another test for sequence
    That is interesting case. Each character has separate sequence. But something is wrong with event handling. Instead of emitting SEQREKSIO:ONFINISHED^1K14 emulator fires SEQREKSIO_ONFINISHED^1K8, which cause infinite loop. So this test shows that problem. Fix in progress.

--- core/tests/pl/genschu/bloomooemulator/tests/SequenceTest.java
+++ core/tests/pl/genschu/bloomooemulator/tests/SequenceTest.java
@@ -14,10 +14,7 @@ import pl.genschu.bloomooemulator.interpreter.Context;
 import pl.genschu.bloomooemulator.interpreter.variable.Attribute;
 import pl.genschu.bloomooemulator.interpreter.variable.Signal;
 import pl.genschu.bloomooemulator.interpreter.variable.Variable;
-import pl.genschu.bloomooemulator.interpreter.variable.types.AnimoVariable;
-import pl.genschu.bloomooemulator.interpreter.variable.types.SequenceVariable;
-import pl.genschu.bloomooemulator.interpreter.variable.types.SoundVariable;
-import pl.genschu.bloomooemulator.interpreter.variable.types.StringVariable;
+import pl.genschu.bloomooemulator.interpreter.variable.types.*;
 import pl.genschu.bloomooemulator.utils.FileUtils;
 
 import java.util.*;
@@ -38,8 +35,9 @@ public class SequenceTest {
 
         public void emitSignal(Variable v, String s, Object arg, Runnable superEmit) {
             if (signals.size() >= eventLimit) throw new RuntimeException("Too many signals");
-            if(arg != null && arg.toString().matches(".*_[0-9]+$")) { // remove event number
-                arg = arg.toString().replaceFirst("_[0-9]+$", "_*");
+            if(arg != null && arg.toString().matches(".*_[0-9]+$")) { // ignore it
+                superEmit.run();
+                return;
             }
             if(!s.contains("ONFRAMECHANGED") && (
                     v instanceof CapturingSequence ||
@@ -107,13 +105,7 @@ public class SequenceTest {
         String filename = "kogut.ann";
         kogut.setAttribute("FILENAME", new Attribute("STRING", filename.toUpperCase()));
 
-        try (MockedStatic<FileUtils> fu = Mockito.mockStatic(FileUtils.class)) {
-            String absPath = Gdx.files.internal("../assets/test-assets/koguty/"+filename).file().getAbsolutePath();
-            fu.when(() -> FileUtils.resolveRelativePath(Mockito.any(Variable.class)))
-                    .thenReturn(absPath);
-
-            kogut.init();
-        }
+        loadWithMockedPath("koguty", filename, kogut);
 
         CapturingAnimo kurator = new CapturingAnimo("KURATOR", ctx, 1000);
         ctx.setVariable("KURATOR", kurator);
@@ -121,13 +113,7 @@ public class SequenceTest {
         filename = "kurator.ann";
         kurator.setAttribute("FILENAME", new Attribute("STRING", filename.toUpperCase()));
 
-        try (MockedStatic<FileUtils> fu = Mockito.mockStatic(FileUtils.class)) {
-            String absPath = Gdx.files.internal("../assets/test-assets/koguty/"+filename).file().getAbsolutePath();
-            fu.when(() -> FileUtils.resolveRelativePath(Mockito.any(Variable.class)))
-                    .thenReturn(absPath);
-
-            kurator.init();
-        }
+        loadWithMockedPath("koguty", filename, kurator);
 
         CapturingSequence seq = new CapturingSequence("GADAJA", ctx, 1000);
         ctx.setVariable("GADAJA", seq);
@@ -135,13 +121,7 @@ public class SequenceTest {
         filename = "gadaja.seq";
         seq.setAttribute("FILENAME", new Attribute("STRING", filename.toUpperCase()));
 
-        try (MockedStatic<FileUtils> fu = Mockito.mockStatic(FileUtils.class)) {
-            String absPath = Gdx.files.internal("../assets/test-assets/koguty/"+filename).file().getAbsolutePath();
-            fu.when(() -> FileUtils.resolveRelativePath(Mockito.any(Variable.class)))
-                    .thenReturn(absPath);
-
-            seq.init();
-        }
+        loadWithMockedPath("koguty", filename, seq);
 
         kogut.setSignal("ONFINISHED^PRZEWRACA", new Signal() {
             @Override
@@ -259,5 +239,238 @@ public class SequenceTest {
         );
         
         assertEquals(expected, signals);
+    }
+
+    @Test
+    void testPlaySequence2() {
+        // S65_ZAMEK - sequence split into separate characters
+        signals.clear();
+
+        ctx.setGame(new Game(null, null)); // faking game
+
+        AnimoVariable zabbaAnimo = new AnimoVariable("ANNZABA", ctx);
+        ctx.setVariable("ANNZABA", zabbaAnimo);
+        String filename = "zaba.ann";
+        zabbaAnimo.setAttribute("FILENAME", new Attribute("STRING", filename.toUpperCase()));
+        loadWithMockedPath("s65_Zamek", filename, zabbaAnimo);
+
+        AnimoVariable rexAnimo = new AnimoVariable("ANNREKSIO", ctx);
+        ctx.setVariable("ANNREKSIO", rexAnimo);
+        filename = "reksio.ann";
+        rexAnimo.setAttribute("FILENAME", new Attribute("STRING", filename.toUpperCase()));
+        loadWithMockedPath("s65_Zamek", filename, rexAnimo);
+
+        AnimoVariable kretesAnimo = new AnimoVariable("ANNKRET", ctx);
+        ctx.setVariable("ANNKRET", kretesAnimo);
+        filename = "kret.ann";
+        kretesAnimo.setAttribute("FILENAME", new Attribute("STRING", filename.toUpperCase()));
+        loadWithMockedPath("s65_Zamek", filename, kretesAnimo);
+
+        CapturingSequence zabba = new CapturingSequence("SEQZABBA", ctx, 30);
+        ctx.setVariable("SEQZABBA", zabba);
+
+        CapturingSequence rex = new CapturingSequence("SEQREKSIO", ctx, 30);
+        ctx.setVariable("SEQREKSIO", rex);
+
+        CapturingSequence kretes = new CapturingSequence("SEQKRET", ctx, 30);
+        ctx.setVariable("SEQKRET", kretes);
+
+        filename = "zabba65.seq";
+        zabba.setAttribute("FILENAME", new Attribute("STRING", filename.toUpperCase()));
+        loadWithMockedPath("s65_Zamek", filename, zabba);
+
+        filename = "reks65.seq";
+        rex.setAttribute("FILENAME", new Attribute("STRING", filename.toUpperCase()));
+        loadWithMockedPath("s65_Zamek", filename, rex);
+
+        filename = "kret65.seq";
+        kretes.setAttribute("FILENAME", new Attribute("STRING", filename.toUpperCase()));
+        loadWithMockedPath("s65_Zamek", filename, kretes);
+
+        // simulating game flow
+        zabba.setSignal("ONFINISHED^1K10", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                kretes.fireMethod("PLAY", new StringVariable("", "1K11", ctx));
+            }
+        });
+        zabba.setSignal("ONFINISHED^1K12", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                kretes.fireMethod("PLAY", new StringVariable("", "1K13", ctx));
+            }
+        });
+        kretes.setSignal("ONFINISHED^1K1", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                rex.fireMethod("PLAY", new StringVariable("", "1K2", ctx));
+            }
+        });
+        kretes.setSignal("ONFINISHED^1K3", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                rex.fireMethod("PLAY", new StringVariable("", "1K4", ctx));
+            }
+        });
+        kretes.setSignal("ONFINISHED^1K5", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                rex.fireMethod("PLAY", new StringVariable("", "1K6", ctx));
+            }
+        });
+        kretes.setSignal("ONFINISHED^1K7", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                rex.fireMethod("PLAY", new StringVariable("", "1K8", ctx));
+            }
+        });
+        kretes.setSignal("ONFINISHED^1K9", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                zabba.fireMethod("PLAY", new StringVariable("", "1K10", ctx));
+            }
+        });
+        kretes.setSignal("ONFINISHED^1K11", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                zabba.fireMethod("PLAY", new StringVariable("", "1K12", ctx));
+            }
+        });
+        kretes.setSignal("ONFINISHED^1K13", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                rex.fireMethod("PLAY", new StringVariable("", "1K14", ctx));
+            }
+        });
+        rex.setSignal("ONFINISHED^1K2", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                kretes.fireMethod("PLAY", new StringVariable("", "1K3", ctx));
+            }
+        });
+        rex.setSignal("ONFINISHED^1K4", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                kretes.fireMethod("PLAY", new StringVariable("", "1K5", ctx));
+            }
+        });
+        rex.setSignal("ONFINISHED^1K6", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                kretes.fireMethod("PLAY", new StringVariable("", "1K7", ctx));
+            }
+        });
+        rex.setSignal("ONFINISHED^1K8", new Signal() {
+            @Override
+            public void execute(Object argument) {
+                kretes.fireMethod("PLAY", new StringVariable("", "1K9", ctx));
+            }
+        });
+
+        kretes.fireMethod("PLAY", new StringVariable("", "1K1", ctx));
+
+        // very hacky way to simulate time passing
+        List<String> eventNames = List.of(
+                "1K1", "1K2", "1K3", "1K4", "1K5", "1K6", "1K7", "1K8", "1K9", "1K10", "1K11", "1K12", "1K13", "1K14"
+        );
+
+        Map<String, AnimoVariable> animoMap = Map.ofEntries( // Map.of has a limit of 10 entries, so we use Map.ofEntries
+                Map.entry("1K1", kretesAnimo),
+                Map.entry("1K2", rexAnimo),
+                Map.entry("1K3", kretesAnimo),
+                Map.entry("1K4", rexAnimo),
+                Map.entry("1K5", kretesAnimo),
+                Map.entry("1K6", rexAnimo),
+                Map.entry("1K7", kretesAnimo),
+                Map.entry("1K8", rexAnimo),
+                Map.entry("1K9", kretesAnimo),
+                Map.entry("1K10", zabbaAnimo),
+                Map.entry("1K11", kretesAnimo),
+                Map.entry("1K12", zabbaAnimo),
+                Map.entry("1K13", kretesAnimo),
+                Map.entry("1K14", rexAnimo)
+        );
+
+        Map<String, CapturingSequence> seqMap = Map.ofEntries(
+                Map.entry("1K1", kretes),
+                Map.entry("1K2", rex),
+                Map.entry("1K3", kretes),
+                Map.entry("1K4", rex),
+                Map.entry("1K5", kretes),
+                Map.entry("1K6", rex),
+                Map.entry("1K7", kretes),
+                Map.entry("1K8", rex),
+                Map.entry("1K9", kretes),
+                Map.entry("1K10", zabba),
+                Map.entry("1K11", kretes),
+                Map.entry("1K12", zabba),
+                Map.entry("1K13", kretes),
+                Map.entry("1K14", rex)
+        );
+
+        for (String event : eventNames) {
+            AnimoVariable animo = animoMap.get(event);
+            CapturingSequence seq = seqMap.get(event);
+            if (animo != null) {
+                animo.fireMethod("STOP");
+            }
+            if (seq != null && seq.getEventsByName().containsKey(event)) {
+                Variable sound = seq.getEventsByName().get(event).getSound();
+                if (sound != null) {
+                    sound.fireMethod("STOP", new BoolVariable("", true, ctx));
+                }
+            }
+            if (animo != null) {
+                animo.fireMethod("STOP");
+            }
+        }
+
+        // 1K14 to podsekwencja ze czterema animacjami, więc zatrzymujemy ją cztery razy
+        for (int i = 0; i < 4; i++) {
+            rexAnimo.fireMethod("STOP");
+        }
+
+        List<String> expected = List.of(
+                "SEQKRET_ONSTARTED^1K1",
+                "SEQKRET_ONFINISHED^1K1",
+                "SEQREKSIO_ONSTARTED^1K2",
+                "SEQREKSIO_ONFINISHED^1K2",
+                "SEQKRET_ONSTARTED^1K3",
+                "SEQKRET_ONFINISHED^1K3",
+                "SEQREKSIO_ONSTARTED^1K4",
+                "SEQREKSIO_ONFINISHED^1K4",
+                "SEQKRET_ONSTARTED^1K5",
+                "SEQKRET_ONFINISHED^1K5",
+                "SEQREKSIO_ONSTARTED^1K6",
+                "SEQREKSIO_ONFINISHED^1K6",
+                "SEQKRET_ONSTARTED^1K7",
+                "SEQKRET_ONFINISHED^1K7",
+                "SEQREKSIO_ONSTARTED^1K8",
+                "SEQREKSIO_ONFINISHED^1K8",
+                "SEQKRET_ONSTARTED^1K9",
+                "SEQKRET_ONFINISHED^1K9",
+                "SEQZABBA_ONSTARTED^1K10",
+                "SEQZABBA_ONFINISHED^1K10",
+                "SEQKRET_ONSTARTED^1K11",
+                "SEQKRET_ONFINISHED^1K11",
+                "SEQZABBA_ONSTARTED^1K12",
+                "SEQZABBA_ONFINISHED^1K12",
+                "SEQKRET_ONSTARTED^1K13",
+                "SEQKRET_ONFINISHED^1K13",
+                "SEQREKSIO_ONSTARTED^1K14",
+                "SEQREKSIO_ONFINISHED^1K14"
+        );
+
+        assertEquals(expected, signals);
+    }
+
+    private void loadWithMockedPath(String assetDirectory, String filename, Variable variable) {
+        try (MockedStatic<FileUtils> fu = Mockito.mockStatic(FileUtils.class)) {
+            String absPath = Gdx.files.internal("../assets/test-assets/" + assetDirectory + "/" + filename).file().getAbsolutePath();
+            fu.when(() -> FileUtils.resolveRelativePath(Mockito.any(Variable.class)))
+                    .thenReturn(absPath);
+
+            variable.init();
+        }
     }
 }
