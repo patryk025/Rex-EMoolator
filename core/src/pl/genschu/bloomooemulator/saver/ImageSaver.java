ff866e9461de7430f10efb7fd0f1a1850ffefdd5
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2025-07-07 21:39:18 +0200

    fix: Corrected header size in ImageSaver
    Yup, the header should be 36 bytes, not 40 excluding magic bytes, so the file was 4 bytes too long. Closes #18.

--- core/src/pl/genschu/bloomooemulator/saver/ImageSaver.java
+++ core/src/pl/genschu/bloomooemulator/saver/ImageSaver.java
@@ -14,7 +14,7 @@ public class ImageSaver {
     public static void saveScreenshot(Variable variable, String path, byte[] data, int width, int height) {
         String filePath = FileUtils.resolveRelativePath(variable, path);
 
-        byte[] imgData = generateData(data, width, height, 16, 0, null, -2, 0); // -2, temporary fix
+        byte[] imgData = generateData(data, width, height, 16, 0, null, 0, 0);
 
         try (FileOutputStream f = new FileOutputStream(filePath)) {
             f.write(imgData);
@@ -31,20 +31,20 @@ public class ImageSaver {
         int imageSize = data.length;
         int alphaSize = alphaData != null ? alphaData.length : 0;
 
-        ByteBuffer buffer = ByteBuffer.allocate(40).order(ByteOrder.LITTLE_ENDIAN);
-        buffer.putInt(width);
-        buffer.putInt(height);
-        buffer.putInt(colorDepth);
-        buffer.putInt(imageSize);
-        buffer.putInt(0); // Zarezerwowane miejsce
-        buffer.putInt(compressionType);
-        buffer.putInt(alphaSize);
-        buffer.putInt(offsetX);
-        buffer.putInt(offsetY);
+        ByteBuffer header = ByteBuffer.allocate(36).order(ByteOrder.LITTLE_ENDIAN);
+        header.putInt(width);
+        header.putInt(height);
+        header.putInt(colorDepth);
+        header.putInt(imageSize);
+        header.putInt(0); // Zarezerwowane miejsce
+        header.putInt(compressionType);
+        header.putInt(alphaSize);
+        header.putInt(offsetX);
+        header.putInt(offsetY);
 
-        ByteBuffer finalBuffer = ByteBuffer.allocate(magicId.length + buffer.capacity() + imageSize + alphaSize);
+        ByteBuffer finalBuffer = ByteBuffer.allocate(magicId.length + header.capacity() + imageSize + alphaSize);
         finalBuffer.put(magicId);
-        finalBuffer.put(buffer.array());
+        finalBuffer.put(header.array());
         finalBuffer.put(data);
         if (alphaData != null) {
             finalBuffer.put(alphaData);
