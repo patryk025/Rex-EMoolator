2cc0edcf54e1fac34fb77f37abc591a45f521267
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2025-11-25 22:17:17 +0100

    test: Added 3 tests to CodeParserTest.java
    For now, 6 vector has problems with parsing, 8 returns wrong result and 11 returns wrong type.

--- core/tests/pl/genschu/bloomooemulator/tests/CodeParserTest.java
+++ core/tests/pl/genschu/bloomooemulator/tests/CodeParserTest.java
@@ -28,7 +28,11 @@ public class CodeParserTest {
 
     @BeforeEach
     void setUp() {
-        ctx = new ContextBuilder().build();
+        ctx = new ContextBuilder()
+                .withFactory("INTEGER", "TEST_NO", 1)
+                .withFactory("INTEGER", "TEST_VALUE_1", 5)
+                .withFactory("STRING", "TEST_VALUE_NAME", "TEST_VALUE_1")
+                .build();
     }
 
     static Stream<Arguments> cases() {
@@ -38,12 +42,15 @@ public class CodeParserTest {
                 arguments("{@RETURN([10%3]);}", 1),
                 arguments("{@RETURN([1.2+2.3]);}", 3.5),
                 arguments("{@INT(\"A\", 5); @RETURN([A + 2]);}", 7),
-                arguments("{@STRING(\"TEST\", \"\");@IF(\"1'1\",\"{TEST^SET(\"OK\");}\",\"{TEST^SET(\"BAD\");}\");}", "OK"),
+                arguments("{@STRING(\"TEST\", \"\");@IF(\"1'1\",\"{TEST^SET(\"OK\");}\",\"{TEST^SET(\"BAD\");}\");@RETURN(TEST);}", "OK"),
                 arguments("{@INT(\"A\",1); @INT(\"A\",[A+1]); @RETURN(A);}", 2),
                 // fibonacci with little subtle changes
                 arguments("{@INT(\"A\",0);@INT(\"B\",1);@INT(\"I\",2);@INT(\"N\",10);@INT(\"TMP\",0);@LOOP(\"{TMP^SET(B);B^SET([A+B]);A^SET(TMP);}\",I,[N-I+1],1);@RETURN(B);}", 55),
-                arguments("{@INT(\"A\",0);@INT(\"B\",1);@INT(\"TMP\",0);@LOOP(\"{TMP^SET(B);B^SET([A+B]);A^SET(TMP);}\",2,9,1);@RETURN(B);}", 55)
-
+                arguments("{@INT(\"A\",0);@INT(\"B\",1);@INT(\"TMP\",0);@LOOP(\"{TMP^SET(B);B^SET([A+B]);A^SET(TMP);}\",2,9,1);@RETURN(B);}", 55),
+                // access to variables by reference
+                arguments("{@RETURN(*TEST_VALUE_NAME^GET());}", 5), // access by STRING variable
+                arguments("{@RETURN(*[\"TEST_VALUE_\"+TEST_NO]^GET());}", 5), // access by arithmetic operation
+                arguments("{@RETURN(*[\"TEST_VALUE_\"+TEST_NO+1]^GET());}", "TEST_VALUE_11") // access to non-existent variable
         );
     }
 
