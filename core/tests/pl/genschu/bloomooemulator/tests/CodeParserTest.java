60d7bd9933af61aec964f0623266478a181269a3
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2025-12-02 14:15:30 +0100

    chore: Little formatting

--- core/tests/pl/genschu/bloomooemulator/tests/CodeParserTest.java
+++ core/tests/pl/genschu/bloomooemulator/tests/CodeParserTest.java
@@ -23,7 +23,7 @@ public class CodeParserTest {
     @BeforeAll
     static void boot() {
         TestEnvironment.init();
-        //TestEnvironment.enableLogs();
+        TestEnvironment.enableLogs();
     }
 
     @BeforeEach
@@ -40,13 +40,48 @@ public class CodeParserTest {
                 arguments("{@RETURN([1+2*3]);}", 7),
                 arguments("{@RETURN([[10-3]*[2+1]]);}", 21),
                 arguments("{@RETURN([10%3]);}", 1),
+                arguments("{@RETURN([2+2*2]);}", 6),
                 arguments("{@RETURN([1.2+2.3]);}", 3.5),
                 arguments("{@INT(\"A\", 5); @RETURN([A + 2]);}", 7),
-                arguments("{@STRING(\"TEST\", \"\");@IF(\"1'1\",\"{TEST^SET(\"OK\");}\",\"{TEST^SET(\"BAD\");}\");@RETURN(TEST);}", "OK"),
-                arguments("{@INT(\"A\",1); @INT(\"A\",[A+1]); @RETURN(A);}", 2),
+                arguments("{" +
+                            "@STRING(\"TEST\", \"\");" +
+                            "@IF(\"1'1\",\"{" +
+                                "TEST^SET(\"OK\");" +
+                            "}\",\"{" +
+                                "TEST^SET(\"BAD\");" +
+                            "}\");" +
+                            "@RETURN(TEST);" +
+                           "}", "OK"),
+                arguments("{" +
+                            "@INT(\"A\",1);" +
+                            "@INT(\"A\",[A+1]); " +
+                            "@RETURN(A);" +
+                          "}", 2),
                 // fibonacci with little subtle changes
-                arguments("{@INT(\"A\",0);@INT(\"B\",1);@INT(\"I\",2);@INT(\"N\",10);@INT(\"TMP\",0);@LOOP(\"{TMP^SET(B);B^SET([A+B]);A^SET(TMP);}\",I,[N-I+1],1);@RETURN(B);}", 55),
-                arguments("{@INT(\"A\",0);@INT(\"B\",1);@INT(\"TMP\",0);@LOOP(\"{TMP^SET(B);B^SET([A+B]);A^SET(TMP);}\",2,9,1);@RETURN(B);}", 55),
+                arguments("{" +
+                            "@INT(\"A\",0);" +
+                            "@INT(\"B\",1);" +
+                            "@INT(\"I\",2);" +
+                            "@INT(\"N\",10);" +
+                            "@INT(\"TMP\",0);" +
+                            "@LOOP(\"{" +
+                                "TMP^SET(B);" +
+                                "B^SET([A+B]);" +
+                                "A^SET(TMP);" +
+                            "}\",I,[N-I+1],1);" +
+                            "@RETURN(B);" +
+                        "}", 55),
+                arguments("{" +
+                            "@INT(\"A\",0);" +
+                            "@INT(\"B\",1);" +
+                            "@INT(\"TMP\",0);" +
+                            "@LOOP(\"{" +
+                                "TMP^SET(B);" +
+                                "B^SET([A+B]);" +
+                                "A^SET(TMP);" +
+                            "}\",2,9,1);" +
+                            "@RETURN(B);" +
+                           "}", 55),
                 // access to variables by reference
                 arguments("{@RETURN(*TEST_VALUE_NAME^GET());}", 5), // access by STRING variable
                 arguments("{@RETURN(*[\"TEST_VALUE_\"+TEST_NO]^GET());}", 5), // access by arithmetic operation
