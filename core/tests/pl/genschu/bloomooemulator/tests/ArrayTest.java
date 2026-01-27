1e014e06e3062dc8282c65a7d8692916f0a745fb
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2026-01-27 22:35:40 +0100

    test: Almost done migration of ArrayTest
    Yup, 3 tests are failing...

--- core/tests/pl/genschu/bloomooemulator/tests/ArrayTest.java
+++ core/tests/pl/genschu/bloomooemulator/tests/ArrayTest.java
@@ -4,15 +4,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.genschu.bloomooemulator.TestEnvironment;
-import pl.genschu.bloomooemulator.builders.LegacyContextBuilder;
-import pl.genschu.bloomooemulator.interpreter.v1.Context;
-import pl.genschu.bloomooemulator.interpreter.v1.variable.Variable;
-import pl.genschu.bloomooemulator.interpreter.v1.variable.types.*;
+import pl.genschu.bloomooemulator.interpreter.values.*;
+import pl.genschu.bloomooemulator.interpreter.variable.ArrayVariable;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayTest {
-    private Context ctx;
     private ArrayVariable arrayVar;
 
     @BeforeAll
@@ -22,234 +21,233 @@ class ArrayTest {
 
     @BeforeEach
     void setUp() {
-        ctx = new LegacyContextBuilder().build();
-        arrayVar = new ArrayVariable("TEST_ARRAY", ctx);
+        arrayVar = new ArrayVariable("TEST_ARRAY");
     }
 
     @Test
     void testAdd() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new DoubleVariable("", 2.5, ctx),
-                new StringVariable("", "text", ctx),
-                new BoolVariable("", true, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new DoubleValue(2.5),
+                new StringValue("text"),
+                new BoolValue(true))).newSelf();
 
-        assertEquals(4, arrayVar.getElements().size());
+        assertEquals(4, arrayVar.elements().size());
 
-        assertEquals("INTEGER", arrayVar.getElements().get(0).getType());
-        assertEquals(1, ((IntegerVariable) arrayVar.getElements().get(0)).GET());
+        assertEquals("INT", arrayVar.elements().get(0).getType().name());
+        assertEquals(1, ((IntValue) arrayVar.elements().get(0)).value());
 
-        assertEquals("DOUBLE", arrayVar.getElements().get(1).getType());
-        assertEquals(2.5, ((DoubleVariable) arrayVar.getElements().get(1)).GET());
+        assertEquals("DOUBLE", arrayVar.elements().get(1).getType().name());
+        assertEquals(2.5, ((DoubleValue) arrayVar.elements().get(1)).value());
 
-        assertEquals("STRING", arrayVar.getElements().get(2).getType());
-        assertEquals("text", ((StringVariable) arrayVar.getElements().get(2)).GET());
+        assertEquals("STRING", arrayVar.elements().get(2).getType().name());
+        assertEquals("text", ((StringValue) arrayVar.elements().get(2)).value());
 
-        assertEquals("BOOL", arrayVar.getElements().get(3).getType());
-        assertTrue(((BoolVariable) arrayVar.getElements().get(3)).GET());
+        assertEquals("BOOL", arrayVar.elements().get(3).getType().name());
+        assertTrue(((BoolValue) arrayVar.elements().get(3)).value());
     }
 
     @Test
     void testGet() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 10, ctx),
-                new StringVariable("", "Hello", ctx),
-                new DoubleVariable("", 3.14, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(10),
+                new StringValue("Hello"),
+                new DoubleValue(3.14))).newSelf();
 
-        Variable element0 = arrayVar.fireMethod("GET", new IntegerVariable("", 0, ctx));
-        Variable element1 = arrayVar.fireMethod("GET", new IntegerVariable("", 1, ctx));
-        Variable element2 = arrayVar.fireMethod("GET", new IntegerVariable("", 2, ctx));
-        Variable element3 = arrayVar.fireMethod("GET", new IntegerVariable("", 3, ctx));
+        Value element0 = arrayVar.callMethod("GET", List.of(new IntValue(0))).getReturnValue();
+        Value element1 = arrayVar.callMethod("GET", List.of(new IntValue(1))).getReturnValue();
+        Value element2 = arrayVar.callMethod("GET", List.of(new IntValue(2))).getReturnValue();
+        Value element3 = arrayVar.callMethod("GET", List.of(new IntValue(3))).getReturnValue();
 
-        assertEquals("INTEGER", element0.getType());
-        assertEquals(10, ((IntegerVariable) element0).GET());
+        assertEquals("INT", element0.getType().name());
+        assertEquals(10, ((IntValue) element0).value());
 
-        assertEquals("STRING", element1.getType());
-        assertEquals("Hello", ((StringVariable) element1).GET());
+        assertEquals("STRING", element1.getType().name());
+        assertEquals("Hello", ((StringValue) element1).value());
 
-        assertEquals("DOUBLE", element2.getType());
-        assertEquals(3.14, ((DoubleVariable) element2).GET());
+        assertEquals("DOUBLE", element2.getType().name());
+        assertEquals(3.14, ((DoubleValue) element2).value());
 
-        assertEquals("STRING", element3.getType());
-        assertEquals("NULL", ((StringVariable) element3).GET());
+        assertEquals("NULL", element3.getType().name());
+        assertEquals("NULL", ((NullValue) element3).toDisplayString());
     }
 
     @Test
     void testGetWithUnknownField() {
-        arrayVar.fireMethod("ADD", new IntegerVariable("", 10, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD", List.of(new IntValue(10))).newSelf();
 
-        Variable result = arrayVar.fireMethod("GET",
-                new IntegerVariable("", 5, ctx),
-                new IntegerVariable("", 0, ctx));
+        Value result = arrayVar.callMethod("GET",
+                List.of(new IntValue(5),
+                new IntValue(0))).getReturnValue();
 
-        assertEquals("STRING", result.getType());
-        assertEquals("NULL", ((StringVariable) result).GET());
+        assertEquals("NULL", result.getType().name());
+        assertEquals("NULL", ((NullValue) result).toDisplayString());
     }
 
     @Test
     void testChangeAt() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new StringVariable("", "old", ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new StringValue("old"))).newSelf();
 
-        arrayVar.fireMethod("CHANGEAT",
-                new IntegerVariable("", 1, ctx),
-                new StringVariable("", "new", ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("CHANGEAT",
+                List.of(new IntValue(1),
+                new StringValue("new"))).newSelf();
 
-        assertEquals("new", ((StringVariable) arrayVar.getElements().get(1)).GET());
+        assertEquals("new", ((StringValue) arrayVar.elements().get(1)).value());
     }
 
     @Test
     void testInsertAt() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new IntegerVariable("", 3, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new IntValue(3))).newSelf();
 
-        arrayVar.fireMethod("INSERTAT",
-                new IntegerVariable("", 1, ctx),
-                new IntegerVariable("", 2, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("INSERTAT",
+                List.of(new IntValue(1),
+                new IntValue(2))).newSelf();
 
-        assertEquals(3, arrayVar.getElements().size());
-        assertEquals(2, ((IntegerVariable) arrayVar.getElements().get(1)).GET());
-        assertEquals(3, ((IntegerVariable) arrayVar.getElements().get(2)).GET());
+        assertEquals(3, arrayVar.elements().size());
+        assertEquals(2, ((IntValue) arrayVar.elements().get(1)).value());
+        assertEquals(3, ((IntValue) arrayVar.elements().get(2)).value());
     }
 
     @Test
     void testRemoveAt() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new IntegerVariable("", 2, ctx),
-                new IntegerVariable("", 3, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new IntValue(2),
+                new IntValue(3))).newSelf();
 
-        arrayVar.fireMethod("REMOVEAT", new IntegerVariable("", 1, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("REMOVEAT", List.of(new IntValue(1))).newSelf();
 
-        assertEquals(2, arrayVar.getElements().size());
-        assertEquals(1, ((IntegerVariable) arrayVar.getElements().get(0)).GET());
-        assertEquals(3, ((IntegerVariable) arrayVar.getElements().get(1)).GET());
+        assertEquals(2, arrayVar.elements().size());
+        assertEquals(1, ((IntValue) arrayVar.elements().get(0)).value());
+        assertEquals(3, ((IntValue) arrayVar.elements().get(1)).value());
     }
 
     @Test
     void testRemoveAll() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new IntegerVariable("", 2, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new IntValue(2))).newSelf();
 
-        arrayVar.fireMethod("REMOVEALL");
+        arrayVar = (ArrayVariable) arrayVar.callMethod("REMOVEALL", List.of()).newSelf();
 
-        assertEquals(0, arrayVar.getElements().size());
+        assertEquals(0, arrayVar.elements().size());
     }
 
     @Test
     void testGetSize() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new IntegerVariable("", 2, ctx),
-                new IntegerVariable("", 3, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new IntValue(2),
+                new IntValue(3))).newSelf();
 
-        IntegerVariable result = (IntegerVariable) arrayVar.fireMethod("GETSIZE");
+        IntValue result = (IntValue) arrayVar.callMethod("GETSIZE", List.of()).getReturnValue();
 
-        assertEquals(3, result.GET());
+        assertEquals(3, result.value());
     }
 
     @Test
     void testFindExactMatch() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new StringVariable("", "test", ctx),
-                new DoubleVariable("", 3.14, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new StringValue("test"),
+                new DoubleValue(3.14))).newSelf();
 
-        IntegerVariable result1 = (IntegerVariable) arrayVar.fireMethod("FIND",
-                new IntegerVariable("", 1, ctx));
+        IntValue result1 = (IntValue) arrayVar.callMethod("FIND",
+                List.of(new IntValue(1))).getReturnValue();
 
-        IntegerVariable result2 = (IntegerVariable) arrayVar.fireMethod("FIND",
-                new StringVariable("", "test", ctx));
+        IntValue result2 = (IntValue) arrayVar.callMethod("FIND",
+                List.of(new StringValue("test"))).getReturnValue();
 
-        IntegerVariable result3 = (IntegerVariable) arrayVar.fireMethod("FIND",
-                new DoubleVariable("", 3.14, ctx));
+        IntValue result3 = (IntValue) arrayVar.callMethod("FIND",
+                List.of(new DoubleValue(3.14))).getReturnValue();
 
-        assertEquals(0, result1.GET());
-        assertEquals(1, result2.GET());
-        assertEquals(2, result3.GET());
+        assertEquals(0, result1.value());
+        assertEquals(1, result2.value());
+        assertEquals(2, result3.value());
     }
 
     @Test
     void testFindNoMatch() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new StringVariable("", "test", ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new StringValue("test"))).newSelf();
 
-        IntegerVariable result = (IntegerVariable) arrayVar.fireMethod("FIND",
-                new DoubleVariable("", 3.14, ctx));
+        IntValue result = (IntValue) arrayVar.callMethod("FIND",
+                List.of(new DoubleValue(3.14))).getReturnValue();
 
-        assertEquals(-1, result.GET());
+        assertEquals(-1, result.value());
     }
 
     @Test
     void testFindSimilarValues() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 4, ctx),
-                new DoubleVariable("", 4.0, ctx),
-                new StringVariable("", "4", ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(4),
+                new DoubleValue(4.0),
+                new StringValue("4"))).newSelf();
 
-        IntegerVariable result1 = (IntegerVariable) arrayVar.fireMethod("FIND",
-                new IntegerVariable("", 4, ctx));
+        IntValue result1 = (IntValue) arrayVar.callMethod("FIND",
+                List.of(new IntValue(4))).getReturnValue();
 
-        IntegerVariable result2 = (IntegerVariable) arrayVar.fireMethod("FIND",
-                new DoubleVariable("", 4.0, ctx));
+        IntValue result2 = (IntValue) arrayVar.callMethod("FIND",
+                List.of(new DoubleValue(4.0))).getReturnValue();
 
-        IntegerVariable result3 = (IntegerVariable) arrayVar.fireMethod("FIND",
-                new StringVariable("", "4", ctx));
+        IntValue result3 = (IntValue) arrayVar.callMethod("FIND",
+                List.of(new StringValue("4"))).getReturnValue();
 
         // it returns 0, no matter what type it is, argument is cast to value type
-        assertEquals(0, result1.GET());
-        assertEquals(0, result2.GET());
-        assertEquals(0, result3.GET());
+        assertEquals(0, result1.value());
+        assertEquals(0, result2.value());
+        assertEquals(0, result3.value());
     }
 
     @Test
     void testContains() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new StringVariable("", "test", ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new StringValue("test"))).newSelf();
 
-        BoolVariable result1 = (BoolVariable) arrayVar.fireMethod("CONTAINS",
-                new StringVariable("", "test", ctx));
+        BoolValue result1 = (BoolValue) arrayVar.callMethod("CONTAINS",
+                List.of(new StringValue("test"))).getReturnValue();
 
-        BoolVariable result2 = (BoolVariable) arrayVar.fireMethod("CONTAINS",
-                new StringVariable("", "missing", ctx));
+        BoolValue result2 = (BoolValue) arrayVar.callMethod("CONTAINS",
+                List.of(new StringValue("missing"))).getReturnValue();
 
-        assertTrue(result1.GET());
-        assertFalse(result2.GET());
+        assertTrue(result1.value());
+        assertFalse(result2.value());
     }
 
     @Test
     void testReverseFind() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 5, ctx),
-                new StringVariable("", "test", ctx),
-                new IntegerVariable("", 5, ctx));
+        arrayVar = (ArrayVariable) arrayVar.callMethod("ADD",
+                List.of(new IntValue(5),
+                new StringValue("test"),
+                new IntValue(5))).newSelf();
 
-        IntegerVariable result = (IntegerVariable) arrayVar.fireMethod("REVERSEFIND",
-                new IntegerVariable("", 5, ctx));
+        IntValue result = (IntValue) arrayVar.callMethod("REVERSEFIND",
+                List.of(new IntValue(5))).getReturnValue();
 
-        assertEquals(2, result.GET());
+        assertEquals(2, result.value());
     }
 
-    @Test
+    /*@Test
     void testClone() {
-        arrayVar.fireMethod("ADD",
-                new IntegerVariable("", 1, ctx),
-                new StringVariable("", "test", ctx));
+        arrayVar.callMethod("ADD",
+                List.of(new IntValue(1),
+                new StringValue("test")));
 
         ArrayVariable clone = arrayVar.clone();
 
         assertNotSame(arrayVar, clone);
-        assertEquals(2, clone.getElements().size());
-        assertEquals(1, ((IntegerVariable) clone.getElements().get(0)).GET());
-        assertEquals("test", ((StringVariable) clone.getElements().get(1)).GET());
+        assertEquals(2, clone.elements().size());
+        assertEquals(1, ((IntValue) clone.elements().get(0)).value());
+        assertEquals("test", ((StringValue) clone.elements().get(1)).value());
 
-        clone.getElements().add(new IntegerVariable("", 3, ctx));
-        assertEquals(2, arrayVar.getElements().size());
-        assertEquals(3, clone.getElements().size());
-    }
+        clone.elements().add(new IntValue(3));
+        assertEquals(2, arrayVar.elements().size());
+        assertEquals(3, clone.elements().size());
+    }*/
 }
