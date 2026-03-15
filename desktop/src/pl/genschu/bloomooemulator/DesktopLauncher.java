946a16d69a1d34994f3e325577ad3d521a110df7
Patryk Gensch <43010113+patryk025@users.noreply.github.com>
2026-03-15 13:00:39 +0100

    fix: Corrected paths to the games.json and moved LWJGL3 to a separate thread on macOS
    Fixes #30

--- desktop/src/pl/genschu/bloomooemulator/DesktopLauncher.java
+++ desktop/src/pl/genschu/bloomooemulator/DesktopLauncher.java
@@ -2,16 +2,27 @@ package pl.genschu.bloomooemulator;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import pl.genschu.bloomooemulator.logic.GameEntry;
+import pl.genschu.bloomooemulator.logic.GameManager;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+import java.io.File;
+
+// On macOS application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main (String[] args) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Rex EMoolator");
+		config.setWindowedMode(800, 600);
 
-		//String testScene = "D:\\Program Files\\AidemMedia\\Reksio i Wehikuł Czasu\\dane\\game\\przygoda\\CS_ZANURZENIEKRETA";
+		GameEntry game = null;
+		if (args.length > 0) {
+			int gameIndex = Integer.parseInt(args[0]);
+			String folderPath = System.getProperty("user.home") + File.separator + ".rexemoolator";
+			GameManager gm = new GameManager(folderPath);
+			game = gm.getGames().get(gameIndex);
+		}
 
-		new Lwjgl3Application(new BlooMooEngine(null), config);
+		new Lwjgl3Application(new BlooMooEngine(game), config);
 	}
 }
